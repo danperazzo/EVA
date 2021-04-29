a1b55e6643d6d27d88cecaef27ad357e9b2ef69e
Jose Roberto Fonseca Jr <jrobertofonsecajr@gmail.com>
2021-04-29 08:32:53 -0300

    add basic styling

--- eva-front/src/app/app.component.ts
+++ eva-front/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { MegaMenuItem } from 'primeng/api'; //required when using MegaMenu
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   items: MenuItem[] = [];
