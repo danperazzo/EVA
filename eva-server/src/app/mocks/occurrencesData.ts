706766b7935db93095920df0bcc0b9e1f8946c2f
Natália Soares <nsoares@mimic.com.br>
2021-05-01 17:27:38 -0300

    Refact occurrences mocks data organization

---
+++ eva-server/src/app/mocks/occurrencesData.ts
@@ -0,0 +1,87 @@
+  const occurrence1 = {
+    "date": "2021-02-25T22:31:09.553Z",
+    "needsMedicalAssistance": "true",
+    "urgencyLevel": "3",
+    "city": "Recife",
+  }
+
+  const occurrence2 = {
+    "date": "2021-03-19T10:45:28.934Z",
+    "needsMedicalAssistance": "true",
+    "needsPsychologicalAssistance": "true",
+    "urgencyLevel": "5",
+    "city": "Recife"
+  }
+
+  const occurrence3 = {
+    "date": "2021-02-10T12:42:07.193Z",
+    "needsSecurityAssistance": "true",
+    "urgencyLevel": "4",
+    "city": "Jaboatão"
+  }
+
+  const occurrence4 = {
+    "date": "2021-04-30T20:43:15.346Z",
+    "needsMedicalAssistance": "true",
+    "needsSecurityAssistance": "true",
+    "urgencyLevel": "5",
+    "city": "Recife"
+  }
+
+  const occurrence5 = {
+    "date": "2021-04-01T13:28:12.233Z",
+    "needsPsychologicalAssistance": "true",
+    "urgencyLevel": "2",
+    "city": "Olinda"
+  }
+
+  const occurrence6 = {
+    "date": "2021-02-13T21:51:14.672Z",
+    "needsSecurityAssistance": "true",
+    "urgencyLevel": "1",
+    "city": "Recife"
+  }
+
+  const occurrence7 = {
+    "date": "2021-03-19T10:45:28.934Z",
+    "needsMedicalAssistance": "true",
+    "needsPsychologicalAssistance": "true",
+    "urgencyLevel": "5",
+    "city": "Jaboatão"
+  }
+
+  const occurrence8 = {
+    "date": "2021-02-10T12:42:07.193Z",
+    "needsSecurityAssistance": "true",
+    "urgencyLevel": "4",
+    "city": "Olinda"
+  }
+
+  const occurrence9 = {
+    "date": "2021-01-13T18:38:17.485Z",
+    "needsSecurityAssistance": "true",
+    "urgencyLevel": "3",
+    "city": "Olinda"
+  }
+
+  const occurrence10 = {
+    "date": "2021-01-27T22:18:53.823Z",
+    "needsMedicalAssistance": "true",
+    "needsSecurityAssistance": "true",
+    "needsPsychologicalAssistance": "true",
+    "urgencyLevel": "5",
+    "city": "Recife"
+  }
+
+  export const occurrencesList = [
+    occurrence1,
+    occurrence2,
+    occurrence3,
+    occurrence4,
+    occurrence5,
+    occurrence6,
+    occurrence7,
+    occurrence8,
+    occurrence9,
+    occurrence10
+  ]
