becff6232a2369668a88406b4989948859f8b4a6
Jose Roberto Fonseca Jr <jrobertofonsecajr@gmail.com>
2021-05-02 16:21:20 -0300

    add home button

--- eva-front/src/app/app.component.ts
+++ eva-front/src/app/app.component.ts
@@ -12,39 +12,13 @@ export class AppComponent {
   items: MenuItem[] = [];
 
   mapsUrl: string = '/maps/hospital&das&clinicas';
-  
+
   ngOnInit() {
     this.items = [
       {
-        label: 'File',
-        icon: 'pi pi-fw pi-file',
-        items: [
-          {
-            label: 'New',
-            icon: 'pi pi-fw pi-plus',
-            items: [
-              {
-                label: 'Bookmark',
-                icon: 'pi pi-fw pi-bookmark',
-              },
-              {
-                label: 'Video',
-                icon: 'pi pi-fw pi-video',
-              },
-            ],
-          },
-          {
-            label: 'Delete',
-            icon: 'pi pi-fw pi-trash',
-          },
-          {
-            separator: true,
-          },
-          {
-            label: 'Export',
-            icon: 'pi pi-fw pi-external-link',
-          },
-        ],
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        routerLink: '/'
       },
       {
         label: 'Edit',
