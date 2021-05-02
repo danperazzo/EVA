5c0129d52dbb29c01b2acd775de0251ffd273c29
Natália Soares <nsoares@mimic.com.br>
2021-05-02 02:07:04 -0300

    Add server-front integration for occurrences counter by urgency per day

--- eva-front/src/app/admin.service.ts
+++ eva-front/src/app/admin.service.ts
@@ -1,36 +1,58 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { retry, map } from "rxjs/operators";
 
-import { Institution } from "../../../common/models";
+import { Occurrence } from "../../../common/models";
 
-//import { MockServer } from "../../../ta-server/ta-server";
+// import OccurrencesController from "../../../eva-server/src/app/controllers/OccurrencesController";
 
 @Injectable()
 export class AdminServices {
   private headers = new HttpHeaders({ "Content-Type": "application/json" });
-  private taURL = "http://localhost:3000";
+  private serverURL = "http://localhost:3333";
 
-//  private server: MockServer;
+  // private server = OccurrencesController;
 
-/*  constructor(private http: HttpClient) {
-    this.server = new MockServer();
+  constructor(private http: HttpClient) {
+    // this.server = OccurrencesController;
   }
-*/
-  filterOccurrencesByYear(year: number) {}
+
+  // countOccurrencesByUrgencyInDate(year: number): Observable<Occurrence[]>{
+  //   let param = new HttpParams().set("yearFilter", year.toString());
+  //   return this.http.get<any>(this.serverURL + "/occurrences/countByUrgencyInDate", {headers: this.headers, params: param})
+  //            .pipe( 
+  //               retry(2)
+  //             ); 
+  // };
+
+  countOccurrencesByUrgencyInDate(date: string) {
+    let param = new HttpParams().set("dateFilter", date);
+    console.log("param =" + param); 
+    return this.http.get(this.serverURL + "/occurrences/countByUrgencyInDate", {headers: this.headers, params: param})
+             .toPromise()
+             .then(res => res)
+             .catch(this.tratarErro);
+  }
+
+  private tratarErro(erro: any): Promise<any>{
+    console.error('Acesso mal sucedido ao serviço de ocorrências',erro);
+    return Promise.reject(erro.message || erro);
+  }
+
   filterOccurrenceByDateRange(from: Date, to: Date) {}
   filterOcurrencesByUrgencyLevel(urgencyLevel: number) {}
 
-  // addInstitution(institution : Institution ):Observable<Institution>{}
-  // deleteInstitution(institution : Institution):Observable<Institution>{}
-  addInstitution(institution: Institution): void {
-    // TODO: query
-  }
-  deleteInstitution(
-    institutionList: Institution[],
-    institutionName: string
-  ): void {}
+
+  // addInstitution(institution: Institution): void {
+  //   // TODO
+  // }
+  // deleteInstitution(
+  //   institutionList: Institution[],
+  //   institutionName: string
+  // ): void {
+  //   // TODO
+  // }
 
   /*
   criar(aluno: Aluno): Observable<Aluno> {
--- eva-front/src/app/admin/admin.component.ts
+++ eva-front/src/app/admin/admin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Occurrence } from  "../../../../common/models";
+import { AdminServices } from '../admin.service';
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -7,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  
+  constructor(private adminServices: AdminServices) {}
+
   activeIndex: number = 0;
-  constructor() { }
+  dateFilter: string = "2021-02-10";
+  occurrenceList: Occurrence[] = [];
+
+  countOccurrencesByUrgencyInDate(date: string){
+    this.adminServices.countOccurrencesByUrgencyInDate(this.dateFilter).then(response => {
+      const json = JSON.stringify(response);
+    console.log(json);
+  });
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
